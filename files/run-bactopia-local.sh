7a6cae0c26dab4390c23577ed2ed76dc1c141942
Micah Halter <micah.halter@gtri.gatech.edu>
2023-08-30 11:39:31 -0400

    fix: update ncbi-genome-download to fix dataset downloading
    Related issue: https://github.com/kblin/ncbi-genome-download/issues/209

--- files/run-bactopia-local.sh
+++ files/run-bactopia-local.sh
@@ -28,11 +28,11 @@ fi
 curl https://datasets.bactopia.com/datasets/v2.2.0/amrfinderdb.tar.gz \
     -o ${hamrdir}/amrfinderdb.tar.gz
 
-date -u +"%Y-%m-%dT%H:%M:%SZ" > ${hamrdir}/amrfinderdb-updated.txt
+date -u +"%Y-%m-%dT%H:%M:%SZ" >${hamrdir}/amrfinderdb-updated.txt
 
-# symlinks don't work here if made for the host and then mounted into the 
+# symlinks don't work here if made for the host and then mounted into the
 # container. so cp just to make things happen. a touch may be sufficient for the
-# tar.gz (instead of copy) as it appears that only the existence of the file is 
+# tar.gz (instead of copy) as it appears that only the existence of the file is
 # checked for in the buggy code
 cp ${hamrdir}/amrfinderdb-updated.txt ${hamrdir}/amrfinder-updated.txt
 cp ${hamrdir}/amrfinderdb.tar.gz ${hamrdir}/amrfinder.tar.gz
@@ -40,13 +40,17 @@ echo "END hack stuff for AMRFinder+ bug in bactopia 2"
 
 echo "Building datasets"
 docker run --volume ${hdatasetdir}:${cdatasetdir} \
-        --name bactopia-datasets-container \
-        bactopia/bactopia bactopia datasets \
-        --outdir ${cdatasetdir} \
-        --species "Staphylococcus aureus" \
-        --include_genus \
-        --limit 100 \
-        --cpus 2
+    --name bactopia-datasets-container \
+    bactopia/bactopia /bin/bash -c "
+        printf '==== Updating ncbi-genome-download ====\n' &&
+        conda install -y -c bioconda ncbi-genome-download=0.3.3 &&
+        printf '\n\n==== Pulling bactopia datasets ====\n' &&
+        bactopia datasets \
+            --outdir ${cdatasetdir} \
+            --species 'Staphylococcus aureus' \
+            --include_genus \
+            --limit 100 \
+            --cpus 1"
 
 docker logs -f bactopia-datasets-container
 
@@ -59,16 +63,15 @@ rm -rf staphylococcus-aureus/
 
 echo "running bactopia on datasets"
 docker run --volume ${hdatasetdir}:${cdatasetdir} --volume ${houtdir}:${coutdir} \
-        --name bactopia-run-container \
-        bactopia/bactopia bactopia --accession SRX4563634 \
-         --datasets ${cdatasetdir} \
-         --species "Staphylococcus aureus" \
-         --coverage 100 \
-         --genome_size median \
-         --outdir ${coutdir}/ena-single-sample \
-         --max_cpus 2
+    --name bactopia-run-container \
+    bactopia/bactopia bactopia --accession SRX4563634 \
+    --datasets ${cdatasetdir} \
+    --species "Staphylococcus aureus" \
+    --coverage 100 \
+    --genome_size median \
+    --outdir ${coutdir}/ena-single-sample \
+    --max_cpus 1
 
 docker logs -f bactopia-run-container
 
 echo "complete"
-
