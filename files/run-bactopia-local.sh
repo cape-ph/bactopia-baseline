a9f26050def081d75e01efc0aad4816a2cddaaed
Drew Pihera <dpihera@gmail.com>
2023-08-30 15:37:44 -0400

    eol chars

--- files/run-bactopia-local.sh
+++ files/run-bactopia-local.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # This script is meant to run through the bactopia (v2) tutorial found here:
-# https://bactopia.github.io/v2.2.0/tutorial/ for the purposes of poking it to 
+# https://bactopia.github.io/v2.2.0/tutorial/ for the purposes of poking it to
 # see what it does such that we can get a little idea of an estimate of what it
-# may cost to run in various AWS architectures. It is not meant to be the best 
-# exmaple of how to write bash or do any other things well. 
+# may cost to run in various AWS architectures. It is not meant to be the best
+# exmaple of how to write bash or do any other things well.
 
 # run with sudo for now
 
